7e0f987900061390176045be39427f21cfb3888d
Tim Cools <tim@soloco.be>
2016-04-03 21:21:16 +0200

    Skip/take/projection returns all records

---
+++ src/Marten.Testing/Bugs/Bug_260_skip_take_projection_query_fails.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Marten.Services;
+using Marten.Testing.Documents;
+using Shouldly;
+using Xunit;
+
+namespace Marten.Testing.Bugs
+{
+    public class Bug_260_skip_take_projection_query_fails : DocumentSessionFixture<NulloIdentityMap>
+    {
+        [Fact]
+        public void thr_wrong_number_of_records_are_returned()
+        {
+            Add100Users();
+
+            var ids = theSession.Query<User>()
+                        .Skip(10)
+                        .Take(10)
+                        .Select(entity => entity.Id)
+                        .ToArray();
+
+            ids.Length.ShouldBe(10);
+        }
+
+        private void Add100Users()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                theSession.Store(new User());
+            }
+            theSession.SaveChanges();
+        }
+    }
+}
